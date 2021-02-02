90f8e4987a14e747d3f49c709b424940e873a346
Firoz kumar <firozbhaikardar21@gmail.com>
2021-02-02 14:15:05 +0530

    dynamic template file using DTL

--- g2/course/views.py
+++ g2/course/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def learn_django(request):
-    return render(request,'course/course1.html')
+    cname='Djnago BABA'
+    duration='37 years'
+    seats='434'
+    details={'x':cname,'y':duration,'z':seats}
+    return render(request,'course/course1.html',context=details)
+
 
 def learn_pyhton(request):
     return render(request,'course/course2.html')
