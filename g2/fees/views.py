428a67302eea09602ff7020e7a42e5eb2e03b33e
Firoz kumar <firozbhaikardar21@gmail.com>
2021-02-02 12:14:06 +0530

    render

---
+++ g2/fees/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def fees_django(request):
+    return render(request,'fees/fees1.html')
+
+def fees_python(request):
+    return  render(request,'fees/fees2.html')
