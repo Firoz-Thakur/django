269ebfa7765f1ca9018e07e158bf849be08a7645
Firoz kumar <firozbhaikardar21@gmail.com>
2021-03-05 22:07:36 +0530

    get and post and csrf token

--- gs34/enroll/form.py
+++ gs34/enroll/form.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class stuReg(forms.Form):
-    name=forms.CharField(widget=forms.PasswordInput)  
-    email=forms.EmailField(disabled=True)
+    name=forms.CharField()  
+    email=forms.EmailField()
 
    # name=forms.CharField(widget=forms.HiddenInput)
    # name=forms.CharField(widget=forms.Textarea)  
   #  name=forms.CharField(widget=forms.CheckboxInput) 
-    name=forms.CharField(widget=forms.FileInput) 
+    #name=forms.CharField(widget=forms.FileInput) 
   
