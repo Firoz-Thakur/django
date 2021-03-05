7ac381e3a9d1deaa8ad74430ac7b53817d39805e
Firoz kumar <firozbhaikardar21@gmail.com>
2021-03-05 13:15:43 +0530

    form fields

---
+++ gs32/enroll/form.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class stuReg(forms.Form):
+    name=forms.CharField(label='Your name')  
+    email=forms.EmailField(disabled=True)
+    mobile=forms.IntegerField(initial='8987389',required=False)
+    key=forms.CharField(widget=forms.HiddenInput())
+    
